a21835a7aec3ec12f2e259826ce5043a10f24e4f
Anwesh43 <anweshthecool0@gmail.com>
2019-06-16 08:17:12 +0530

    adding TreeRotView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtreerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtreerotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtreerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.treerotview.TreeRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TreeRotView.create(this)
     }
 }
