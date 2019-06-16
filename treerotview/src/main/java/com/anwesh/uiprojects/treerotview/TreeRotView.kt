337da84c6a290cfb221dbd9ff4b4cb7ae4b37dca
Anwesh43 <anweshthecool0@gmail.com>
2019-06-16 08:02:36 +0530

    adding TRNode class

--- treerotview/src/main/java/com/anwesh/uiprojects/treerotview/TreeRotView.kt
+++ treerotview/src/main/java/com/anwesh/uiprojects/treerotview/TreeRotView.kt
@@ -132,4 +132,48 @@ class TreeRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TRNode(var i : Int, val state : State = State()) {
+
+        private var next : TRNode? = null
+        private var prev : TRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TRNode {
+            var curr : TRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
