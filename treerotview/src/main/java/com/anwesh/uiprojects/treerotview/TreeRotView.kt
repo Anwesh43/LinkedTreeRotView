532140e78120f8c28e75c92ed39fd6c74c575414
Anwesh43 <anweshthecool0@gmail.com>
2019-06-16 07:49:42 +0530

    adding TreeRotView class

--- treerotview/src/main/java/com/anwesh/uiprojects/treerotview/TreeRotView.kt
+++ treerotview/src/main/java/com/anwesh/uiprojects/treerotview/TreeRotView.kt
@@ -73,3 +73,21 @@ fun Canvas.drawTRNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TreeRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
