bc195db427f230850e1a85769fa33dac71055705
Anwesh43 <anweshthecool0@gmail.com>
2019-06-16 07:34:39 +0530

    adding libraries and constant

--- treerotview/src/main/java/com/anwesh/uiprojects/treerotview/TreeRotView.kt
+++ treerotview/src/main/java/com/anwesh/uiprojects/treerotview/TreeRotView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.treerotview
 /**
  * Created by anweshmishra on 16/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val edges : Int = 2
+val parts : Int = 2
+val scGap : Float = 2.9f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
